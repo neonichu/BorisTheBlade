630375d8a865744835f1bf4e1beb2cd46810ffd0
Boris Bügling <boris@icculus.org>
2014-07-05 14:27:39 +0200

    Initial commit

---
+++ BorisTheBlade/BorisTheBlade.h
@@ -0,0 +1,19 @@
+//
+//  BorisTheBlade.h
+//  BorisTheBlade
+//
+//  Created by Boris Bügling on 05/07/14.
+//  Copyright (c) 2014 Boris Bügling. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for BorisTheBlade.
+FOUNDATION_EXPORT double BorisTheBladeVersionNumber;
+
+//! Project version string for BorisTheBlade.
+FOUNDATION_EXPORT const unsigned char BorisTheBladeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BorisTheBlade/PublicHeader.h>
+
+
---
+++ README.md
@@ -0,0 +1,5 @@
+Framework for using Xcode Playgrounds in your own app.
+
+## Help needed
+
+Follow [@NeoNacho](https://twitter.com/NeoNacho) to help me beat [@orta](https://twitter.com/orta) in followers count.
